1c3f79a0b1cfff9455b3e1dbb0a920ca12a0a9dc
kon1z <zzx2867199593@qq.com>
2024-01-27 23:40:02 +0800

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,26 @@
+import { Environment } from '@abp/ng.core';
+
+const baseUrl = 'http://localhost:4200';
+
+export const environment = {
+  production: false,
+  application: {
+    baseUrl,
+    name: 'Blog',
+    logoUrl: '',
+  },
+  oAuthConfig: {
+    issuer: 'https://localhost:44340/',
+    redirectUri: baseUrl,
+    clientId: 'Blog_App',
+    responseType: 'code',
+    scope: 'offline_access Blog',
+    requireHttps: true,
+  },
+  apis: {
+    default: {
+      url: 'https://localhost:44350',
+      rootNamespace: 'Acme.Blog',
+    },
+  },
+} as Environment;
